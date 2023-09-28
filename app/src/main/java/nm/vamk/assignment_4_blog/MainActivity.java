b2733ce53c0e858ce15ad1ab123ffc85ae8bdec6
Niko Myllymäki <niko.myllymaki1@gmail.com>
2023-09-28 13:58:40 +0300

    Set user input to text field.

--- app/src/main/java/nm/vamk/assignment_4_blog/MainActivity.java
+++ app/src/main/java/nm/vamk/assignment_4_blog/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,22 +30,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private View.OnClickListener submitButtonClickListener = new View.OnClickListener() {
+        String username = "";
+        String comment = "";
         @Override
         public void onClick(View v) {
             Button submitButton = (Button) v;
             if(usernameEditText.getText().length() == 0 ) {
                 usernameEditText.setBackgroundColor(Color.rgb(255,0,0));
             } else {
-                textFieldTextView.setText(usernameEditText.getText());
+                username = String.valueOf(usernameEditText.getText());
 
             }
 
             if(commentEditText.getText().length() == 0) {
                 commentEditText.setBackgroundColor(Color.rgb(255,0,0));
             } else {
-                textFieldTextView.setText(commentEditText.getText());
-                //Currently overwrites the text from usernameEditText
+                comment = String.valueOf(commentEditText.getText());
             }
+
+            if(username.isEmpty() || comment.isEmpty()) {
+                textFieldTextView.setText("");
+            } else {
+                textFieldTextView.setText(username + " " + comment);
+                usernameEditText.setBackgroundColor(getResources().getColor(R.color.white));
+                commentEditText.setBackgroundColor(getResources().getColor(R.color.white));
+            }
+
+
 
         }
     };
