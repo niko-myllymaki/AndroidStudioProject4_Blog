cea069e1411f2fdbf064e4c278c525210aca394c
Niko Myllymäki <niko.myllymaki1@gmail.com>
2023-10-03 19:32:20 +0300

    Comment data shows correctly in text view, text search works, date search needs some fixing.

--- app/src/main/java/nm/vamk/assignment_4_blog/MainActivity.java
+++ app/src/main/java/nm/vamk/assignment_4_blog/MainActivity.java
@@ -10,17 +10,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-
     EditText usernameEditText;
     EditText commentEditText;
     TextView textFieldTextView;
-
-    TextView textFieldTextViewTemp;
+    EditText textToSearchEditText;
+    EditText dateToSearchEditText;
+    TextView searchResult;
+    ArrayList<String> commentData;
+    StringBuilder stringBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,23 +35,31 @@ public class MainActivity extends AppCompatActivity {
         usernameEditText = findViewById(R.id.editText_username);
         commentEditText = findViewById(R.id.editText_comment);
         textFieldTextView = findViewById(R.id.tw_textField);
+        commentData = new ArrayList<>();
+        stringBuilder = new StringBuilder();
+
 
         Button submitButton = findViewById(R.id.button_submit);
         submitButton.setOnClickListener(submitButtonClickListener);
+
+        Button searchButton = findViewById(R.id.button_search);
+        searchButton.setOnClickListener(searchButtonButtonClickListener);
     }
 
     private View.OnClickListener submitButtonClickListener = new View.OnClickListener() {
         String username = "";
         String comment = "";
+        int entryNumber = 1;
+
+        String commentDataToSave;
+
         @Override
         public void onClick(View v) {
-            Button submitButton = (Button) v;
             if(usernameEditText.getText().length() == 0 ) {
                 usernameEditText.setBackgroundColor(Color.rgb(255,0,0));
             } else {
                 username = String.valueOf(usernameEditText.getText());
                 usernameEditText.setText("");
-
             }
 
             if(commentEditText.getText().length() == 0) {
@@ -59,20 +72,93 @@ public class MainActivity extends AppCompatActivity {
             if(username.isEmpty() || comment.isEmpty()) {
                 textFieldTextView.setText("");
             } else {
-                textFieldTextView.setText(getCurrentDateTime() + " " + username + " - " + comment);
-                textFieldTextViewTemp = textFieldTextView;
+                String entryNumberString = String.valueOf(entryNumber ++);
+                commentDataToSave = getCurrentDateTime() + " " + username + " - " + comment;
+                commentData.add(entryNumberString + " " + commentDataToSave);
                 usernameEditText.setBackgroundColor(0);
                 commentEditText.setBackgroundColor(0);
+
+
+                for(int i = commentData.size() - 1; i >=0; i--) {
+                    stringBuilder.append(commentData.get(i)).append("\n");
+                    Log.d("Comments", commentData.get(i));
+                }
+
+                textFieldTextView.setText(stringBuilder.toString());
+                stringBuilder.setLength(0);
+
+                 /*
+                textFieldTextViewTemp = textFieldTextView;
+                textFieldTextView.setText(entryNumber++ +". "+ getCurrentDateTime() + " " + username + " - " + comment + "\n"
+                        + textFieldTextViewTemp.getText().toString());
+
+                if(commentData.size() <= 1) {
+                    Log.d("Comments", commentData.get(0));
+                } else {
+
+                }
+
+                  */
+
+
             }
+            //newComment = textFieldTextView;
+            //currentComment = textFieldTextView;
+            //textFieldTextView.setText(currentComment.getText() + " | " +newComment.getText());
+
+            /*
+            textFieldData = textFieldTextView.getText().toString();
+            userEntries.add(textFieldData);
+            */
+
+            //Log.d("check", userEntries.get(0) + userEntries.get(1));
+
+            /*
             textFieldTextViewTemp = textFieldTextView;
             textFieldTextView.setText(textFieldTextViewTemp.getText());
 
             String tempValue = (String) textFieldTextView.getText();
             Log.d("check", tempValue);
+            */
 
 
 
+        }
+    };
 
+    private View.OnClickListener searchButtonButtonClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            textToSearchEditText = findViewById(R.id.editText_textToSearch);
+            dateToSearchEditText = findViewById(R.id.editText_dateToSearch);
+
+            String searchText = textToSearchEditText.getText().toString();
+            String searchDate = dateToSearchEditText.getText().toString();
+            StringBuilder searchResults = new StringBuilder();
+            searchResult = findViewById(R.id.tw_searchResult);
+            searchResult.setText("");
+
+            //Need to fix the searching by date!
+            for(String comment : commentData) {
+                if(comment.toLowerCase().contains(searchText.toLowerCase()) || comment.toLowerCase().contains(searchDate)) {
+                    searchResults.append(comment).append("\n");
+                }
+            }
+
+            if (searchResults.length() > 0) {
+                searchResult.setText(searchResults.toString());
+            } else {
+                searchResult.setText("No matching results found.");
+            }
+
+
+            /*
+            if(commentData.contains(textToSearchEditText.getText().toString())) {
+                searchResult.setText(commentData.get());
+            } else {
+                Log.d("button", "Nothing...");
+            }
+             */
+            //searchResult.setText(textToSearchEditText.getText());
         }
     };
 
